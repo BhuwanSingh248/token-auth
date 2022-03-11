f34b06a767dabcd9b9f76f4b22a22e444c036ec5
bhuwansingh248 <bhuwansingh248@gmail.com>
2022-03-11 18:05:48 +0530

    (@) configured refresh token for ui

--- ui/src/context/AuthContext.js
+++ ui/src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import jwt_decode from "jwt-decode"
 import { useNavigate } from 'react-router-dom'
 const AuthContext = createContext()
@@ -17,6 +17,8 @@ export const AuthProvider = ({children}) =>{
             localStorage.getItem('authToken')
             ):null
     )
+
+    let [loading, setLoading] = useState(true)
 
     //  setting up token auth from local storage
     const history = useNavigate()
@@ -52,12 +54,49 @@ export const AuthProvider = ({children}) =>{
         history(to='/login')
     }
 
+    let updateToken = async () => {
+        console.log('token updated')
+        let response = await fetch('http://localhost:8000/api/token/refresh/',{
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },  
+            body:JSON.stringify({
+                'refresh':authToken.refresh
+            })
+          })
+          let data = await response.json()
+          if(response.status === 200){
+            setAuthToken(data)
+            setUser(jwt_decode(data.access))
+            localStorage.setItem('authToken', JSON.stringify(data))
+          }else{
+              logoutUser()
+          }
+    }
+
 
     let contextData = {
         user:user,
         loginUser:loginUser,
         logoutUser:logoutUser
     }
+
+    useEffect(() => {
+        let updateTime = (t) => {
+            return 1000* 60 * t
+        }
+        let interval = setInterval(() => {
+            if(authToken){
+                updateToken()
+            }
+        }, updateTime(5))
+
+        return() => clearInterval(interval)
+
+    }, [authToken, loading])
+
+    // ----------------------------------------------
     return(
         <AuthContext.Provider value={contextData}>
             {children}
