386f9e30cabafd0f2be853db287068d663cee013
bhuwan singh panora <bhuwansingh248@gmail.com>
2026-08-14 12:38:10 +0530

    Expose logout and current-user endpoints

--- applications/base/api/urls.py
+++ applications/base/api/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-from .views import MyTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from .views import LogoutView, MeView, MyTokenObtainPairView
 
 urlpatterns = [
-    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
+    path("token/", MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/logout/", LogoutView.as_view(), name="token_logout"),
+    path("me/", MeView.as_view(), name="me"),
 ]
